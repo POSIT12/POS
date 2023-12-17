f9e4919a8f9117d259a9088073abf68a6551f5fc
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-12-17 11:25:20 +0800

    asda

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -74,8 +74,19 @@ class ApiController extends Controller
             $code = 'SLS'.date('Y').date('m').date('d')."-".str_pad((Sale::count()+1), 4, '0', STR_PAD_LEFT);  
             $lists = $request->lists;
 
+            if(!$request->customer_id){
+                $customer = new Customer;
+                $customer->name = $request->name;
+                $customer->email = $request->email;
+                $customer->contact = $request->contact;
+                $customer->save();
+
+                $customer_id = $customer->id;
+            }else{
+                $customer_id = $request->customer_id;
+            }
             
-            $data = Sale::create(array_merge($request->all(),['code' => $code]));
+            $data = Sale::create(array_merge($request->all(),['code' => $code, 'customer_id' => $customer_id]));
             if($data){
                 foreach($lists as $list){
                     $l = new SaleList;
